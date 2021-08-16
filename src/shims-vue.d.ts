f8d8be4adb2e4e41be4862b072c4b39ec4ef32fe
Evllis <7284395@qq.com>
2021-08-16 23:24:33 +0800

    ➕ heavy_plus_sign(单元测试): 添加单元测试功能(暂未完成添加)

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,6 +1,23 @@
-declare module '*.vue' {
-    import { DefineComponent } from 'vue'
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
-    const component: DefineComponent<{}, {}, any>
-    export default component
-}
+// declare module '*.vue' {
+//     import { DefineComponent } from 'vue'
+//     // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
+//     const component: DefineComponent<{}, {}, any>
+//     export default component
+// }
+
+// shims-vue.d.ts
+// import Vue from 'vue'
+// import VueRouter, { Route } from 'vue-router'
+// import { Store } from 'vuex'
+
+// declare module 'vue/types/vue' {
+//     interface Vue {
+//         $router: VueRouter
+//         $route: Route
+//         $store: Store<any>
+//         // 以下是在main.ts中挂载到Vue.prototype上的变量
+//         $api: any
+//         $mock: any
+//         $configs: any
+//     }
+// }
